193266dcba2eadeabf9726ef841d33ea8ad94cac
aa <noamr@dataloop.ai>
2020-01-22 15:46:48 +0200

    bug fix in plugin push

--- launch_pad/launcher.py
+++ launch_pad/launcher.py
@@ -206,11 +206,12 @@ class Launcher:
         dataset_input = dl.FunctionIO(type='Dataset', name='dataset')
         hp_value_input = dl.FunctionIO(type='Json', name='hp_values')
         model_specs_input = dl.FunctionIO(type='Json', name='model_specs')
+        input_to_init = dl.FunctionIO(type='Json', name='package_name', value=package_name)
 
         inputs = [dataset_input, hp_value_input, model_specs_input]
         function = dl.PackageFunction(name='run', inputs=inputs, outputs=[], description='')
         module = dl.PackageModule(entry_point='service_executor.py', name='service_executor', functions=[function],
-                                  init_inputs={'package_name': package_name})
+                                  init_inputs=[input_to_init])
 
         package = self.project.packages.push(
             package_name=package_name,
