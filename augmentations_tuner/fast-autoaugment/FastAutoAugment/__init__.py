8dbe934c06612dd7917f38090701e3ead0337fb8
NoamRosenberg <noamsrosenberg@gmail.com>
2020-08-25 22:54:40 +0800

    refactor fastautoaug

--- augmentations_tuner/fast-autoaugment/FastAutoAugment/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import archive
-from . import data
-from . import train
-from . import augmentations
-from . import common
-from . import aug_mixup
--- augmentations_tuner/fast-autoaugment/FastAutoAugment/networks/efficientnet_pytorch/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.5.1"
-from .model import EfficientNet, RoutingFn
-from .utils import (
-    GlobalParams,
-    BlockArgs,
-    BlockDecoder,
-    efficientnet,
-    get_model_params,
-)
