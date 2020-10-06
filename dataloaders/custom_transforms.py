aeda70e6b7c7d7c635ec15083dbe39a04602efe1
NoamRosenberg <noamsrosenberg@gmail.com>
2020-10-06 14:25:35 +0300

    translateX and translateX bbox both checked

--- dataloaders/custom_transforms.py
+++ dataloaders/custom_transforms.py
@@ -73,7 +73,7 @@ class Translate_Y(object):
         img, annot = sample['img'], sample['annot']
 
         bbs = BoundingBoxesOnImage([BoundingBox(x1=ann[0], y1=ann[1], x2=ann[2], y2=ann[3], label=str(int(ann[4]))) for ann in annot], shape=img.shape)
-        aug = iaa.geometric.TranslateY(percent=0.1)
+        aug = iaa.geometric.TranslateY(percent=self.v)
         img_aug, bbs_aug = aug(image=img, bounding_boxes=bbs)
         annot_aug = np.array([[bb.x1, bb.y1, bb.x2, bb.y2, np.float32(bb.label)] for bb in bbs_aug])
 
@@ -89,12 +89,42 @@ class Translate_Y_BBoxes(object):
         unique_labels = np.unique(annot[:, 4].astype('int').astype('str')).tolist()
 
         bbs = BoundingBoxesOnImage([BoundingBox(x1=ann[0], y1=ann[1], x2=ann[2], y2=ann[3], label=str(int(ann[4]))) for ann in annot], shape=img.shape)
-        aug = iaa.BlendAlphaBoundingBoxes(labels=unique_labels, foreground=iaa.geometric.TranslateY(percent=0.1))
+        aug = iaa.BlendAlphaBoundingBoxes(labels=unique_labels, foreground=iaa.geometric.TranslateY(percent=self.v))
         img_aug, bbs_aug = aug(image=img, bounding_boxes=bbs)
         annot_aug = np.array([[bb.x1, bb.y1, bb.x2, bb.y2, np.float32(bb.label)] for bb in bbs_aug])
         # drawn_img = bbs_aug.draw_on_image(img_aug * 255, size=2, color=[0, 255., 0])
         # import skimage
         # skimage.io.imsave('draw.png', drawn_img)
+        return {'image': img_aug, 'annot': annot_aug}
+
+
+class Translate_X(object):
+    def __init__(self, v):
+        self.v = v
+
+    def __call__(self, sample):
+        img, annot = sample['img'], sample['annot']
+
+        bbs = BoundingBoxesOnImage([BoundingBox(x1=ann[0], y1=ann[1], x2=ann[2], y2=ann[3], label=str(int(ann[4]))) for ann in annot], shape=img.shape)
+        aug = iaa.geometric.TranslateX(percent=self.v)
+        img_aug, bbs_aug = aug(image=img, bounding_boxes=bbs)
+        annot_aug = np.array([[bb.x1, bb.y1, bb.x2, bb.y2, np.float32(bb.label)] for bb in bbs_aug])
+
+        return {'image': img_aug, 'annot': annot_aug}
+
+class Translate_X_BBoxes(object):
+    def __init__(self, v):
+        self.v = v
+
+    def __call__(self, sample):
+        img, annot = sample['img'], sample['annot']
+        unique_labels = np.unique(annot[:, 4].astype('int').astype('str')).tolist()
+
+        bbs = BoundingBoxesOnImage([BoundingBox(x1=ann[0], y1=ann[1], x2=ann[2], y2=ann[3], label=str(int(ann[4]))) for ann in annot], shape=img.shape)
+        aug = iaa.BlendAlphaBoundingBoxes(labels=unique_labels, foreground=iaa.geometric.TranslateX(percent=self.v))
+        img_aug, bbs_aug = aug(image=img, bounding_boxes=bbs)
+        annot_aug = np.array([[bb.x1, bb.y1, bb.x2, bb.y2, np.float32(bb.label)] for bb in bbs_aug])
+
         return {'image': img_aug, 'annot': annot_aug}
 
 
