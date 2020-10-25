d916ac8193f7533ada42d462935828575dbf88ac
NoamRosenberg <noamsrosenberg@gmail.com>
2020-10-25 23:53:48 +0200

    add checkpoint path

--- pred_main.py
+++ pred_main.py
@@ -7,12 +7,13 @@ if __name__ == '__main__':
         parser = argparse.ArgumentParser()
         parser.add_argument("--predict", action='store_true', default=False)
         parser.add_argument("--visualize_coco", action='store_true', default=False)
+        parser.add_argument("--checkpoint_path", type=str, default='/home/noam/ZazuML/best_checkpoint.pt')
         parser.add_argument("--dataset_path", type=str, default='')
         parser.add_argument("--output_path", type=str, default='')
         args = parser.parse_args()
         if args.predict:
                 predict(pred_on_path=args.dataset_path, output_path=args.output_path,
-                checkpoint_path='/home/noam/ZazuML/best_checkpoint.pt', threshold=0.05)
+                checkpoint_path=args.checkpoint_path, threshold=0.5)
         if args.visualize_coco:
                 from dataloaders import CocoDataset
                 dataset = CocoDataset(args.dataset_path, set_name='train')
