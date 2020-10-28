9528abb8fcb4725630477255ec8bd0cca45ace98
loxacom123 <loxacom123@gmail.com>
2020-10-28 17:39:38 +0800

    fix logging

--- logging_utils.py
+++ logging_utils.py
@@ -2,31 +2,31 @@ import logging
 import sys
 import os
 
+LOGGING_FORMAT = '[%(levelname)s] %(asctime)s %(name)s : %(message)s'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
 def init_logging(module_name, filename='logger.conf'):
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT, datefmt=DATE_FORMAT)
     logger = logging.getLogger(module_name)
 
     if os.path.exists(filename):
         os.remove(filename)
-    fileHandler = logging.FileHandler(filename)
-    logger.addHandler(fileHandler)
 
-    consoleHandler = logging.StreamHandler()
-    logger.addHandler(consoleHandler)
+    fileHandler = logging.FileHandler(filename)
+    fileHandler.setFormatter(logging.Formatter(fmt=LOGGING_FORMAT, datefmt=DATE_FORMAT))
+    logger.addHandler(fileHandler)
 
     return logger
 
 def logginger(module_name, filename='logger.conf'):
-    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format=LOGGING_FORMAT, datefmt=DATE_FORMAT)
     logger = logging.getLogger(module_name)
 
-    fileHandler = logging.FileHandler(filename)
-    logger.addHandler(fileHandler)
-
-    consoleHandler = logging.StreamHandler()
-    logger.addHandler(consoleHandler)
-
+    if len(logger.handlers) < 1:
+        fileHandler = logging.FileHandler(filename)
+        fileHandler.setFormatter(logging.Formatter(fmt=LOGGING_FORMAT, datefmt=DATE_FORMAT))
+        logger.addHandler(fileHandler)
+        
     return logger
 
 
